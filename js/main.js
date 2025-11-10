ea629f1f3e96bbe9351f6ab6c281cf7d612e6f0a
Muhammad Keanu <m.keanusulthansyah@gmail.com>
2025-11-10 11:58:24 +0700

    new labels

--- js/main.js
+++ js/main.js
@@ -12,12 +12,12 @@ container.innerHTML = posts.map(post => `
         <span class="relative group inline-block">
           <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"
             stroke-width="1.5" stroke="currentColor"
-            class="w-4 h-4 text-blue-400">
+            class="w-4 h-4 text-black-400">
             <path stroke-linecap="round" stroke-linejoin="round"
               d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.04 5.135a.563.563 0 0 0 .475.347l5.404.406a.563.563 0 0 1 .32.982l-4.118 3.453a.563.563 0 0 0-.182.557l1.28 5.272a.563.563 0 0 1-.84.61l-4.646-2.826a.563.563 0 0 0-.586 0l-4.646 2.826a.563.563 0 0 1-.84-.61l1.28-5.272a.563.563 0 0 0-.182-.557L2.281 10.37a.563.563 0 0 1 .32-.982l5.404-.406a.563.563 0 0 0 .475-.347l2.04-5.135z" />
           </svg>
           <span class="absolute top-full left-1/2 -translate-x-1/2 mt-1 bg-black text-white text-xs px-2 py-1 rounded opacity-0 translate-y-1 group-hover:opacity-100 group-hover:translate-y-0 transition-all whitespace-nowrap shadow-md pointer-events-none">
-            Ditandai oleh Keanu
+            Marked by Keanu
           </span>
         </span>
       ` : ''}
@@ -26,12 +26,23 @@ container.innerHTML = posts.map(post => `
         <span class="relative group inline-block">
           <img src="./assets/explicit.svg" alt="Explicit Content" class="w-4 h-4" />
           <span class="absolute top-full left-1/2 -translate-x-1/2 mt-1 bg-black text-white text-xs px-2 py-1 rounded opacity-0 translate-y-1 group-hover:opacity-100 group-hover:translate-y-0 transition-all whitespace-nowrap shadow-md pointer-events-none">
-            Konten eksplisit
+            Explicit content
+          </span>
+        </span>
+      ` : ''}
+
+            ${post.audio ? `
+        <span class="relative group inline-block">
+          <img src="./assets/audio.svg" alt="Audio Available" class="w-4 h-4" />
+          <span class="absolute top-full left-1/2 -translate-x-1/2 mt-1 bg-black text-white text-xs px-2 py-1 rounded opacity-0 translate-y-1 group-hover:opacity-100 group-hover:translate-y-0 transition-all whitespace-nowrap shadow-md pointer-events-none">
+            Audio Available
           </span>
         </span>
       ` : ''}
     </div>
   </div>
+
+  
 
   <p class="text-sm text-gray-500 mb-3">${post.date}</p>
   <p class="text-gray-500 text-bold mt-1">${post.excerpt}</p>
--- js/post.js
+++ js/post.js
@@ -10,10 +10,45 @@ if (!postId) {
       document.title = `${post.title} - wkeanu`;
       const article = document.getElementById('post');
       article.innerHTML = `
-        <h1 class="text-3xl md:text-4xl font-bold mb-2">${post.title}</h1>
-        <p class="text-gray-500 mb-8">${post.date}</p>
-        <div class="text-gray-800 leading-relaxed space-y-4">${post.content}</div>
-      `;
+  <div class="flex items-center gap-2 mb-8">
+
+    ${post.starred ? `
+      <div class="flex items-center gap-2 bg-white-50 border border-black-400 
+                  text-black w-fit px-3 py-1 rounded-lg">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"
+            stroke-width="1.5" stroke="currentColor"
+            class="w-4 h-4 text-black-400">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.04 5.135a.563.563 0 0 0 .475.347l5.404.406a.563.563 0 0 1 .32.982l-4.118 3.453a.563.563 0 0 0-.182.557l1.28 5.272a.563.563 0 0 1-.84.61l-4.646-2.826a.563.563 0 0 0-.586 0l-4.646 2.826a.563.563 0 0 1-.84-.61l1.28-5.272a.563.563 0 0 0-.182-.557L2.281 10.37a.563.563 0 0 1 .32-.982l5.404-.406a.563.563 0 0 0 .475-.347l2.04-5.135z" />
+          </svg>
+        <span class="text-sm">Marked by Keanu</span>
+      </div>
+    ` : ''}
+
+    ${post.explicit ? `
+      <div class="flex items-center gap-2 bg-white-50 border border-black-400 
+                  text-black w-fit px-3 py-1 rounded-lg">
+        <img src="./assets/explicit.svg" alt="Explicit Content" class="w-4 h-4" />
+        <span class="text-sm">Explicit content</span>
+      </div>
+    ` : ''}
+
+        ${post.audio ? `
+      <div class="flex items-center gap-2 bg-white-50 border border-black-400 
+                  text-black w-fit px-3 py-1 rounded-lg">
+        <img src="./assets/audio.svg" alt="Audio available" class="w-4 h-4" />
+        <span class="text-sm">Audio available</span>
+      </div>
+    ` : ''}
+  </div>
+
+  <h1 class="text-3xl md:text-4xl font-bold mb-2">${post.title}</h1>
+  <p class="text-gray-500 mb-8">${post.date}</p>
+
+  <div class="text-gray-800 leading-relaxed space-y-4 border-t pt-8">
+    ${post.content}
+  </div>
+`;
       setTimeout(() => article.classList.add("opacity-100"), 50);
     })
     .catch(() => {
